1a4f61478a00dbecf06495ed7c16db9812859741
Federico <para.fede.rivero@gmail.com>
2017-11-23 09:46:56 -0200

    Camera roll (Q-E keys)

--- js/components/firstPersonFlyInputComponent.js
+++ js/components/firstPersonFlyInputComponent.js
@@ -14,24 +14,32 @@ class FirstPersonFlyInputComponent extends InputComponent {
 		}
 		var rotationSpeed = 0;
 		if (keyStatus[KeyEnum.LEFT].pressed || keyStatus[KeyEnum.RIGHT].pressed || 
-			keyStatus[KeyEnum.UP].pressed || keyStatus[KeyEnum.DOWN].pressed){
+			keyStatus[KeyEnum.UP].pressed || keyStatus[KeyEnum.DOWN].pressed || keyStatus[KeyEnum.Q].pressed
+			|| keyStatus[KeyEnum.E].pressed){
 			rotationSpeed = 1;
 		}
 
 		var angularSpeedDirection = v3.create(0,0,0);
 		var upDirection = this.owner.getUpDirection();
 		var leftDirection = this.owner.getLeftDirection();
+		var frontDirection = this.owner.getFrontDirection();
 		if (keyStatus[KeyEnum.LEFT].pressed){
 			v3.subtract(angularSpeedDirection, upDirection, angularSpeedDirection);
-			//angularSpeedDirection[1] = -1;
-		}else if (keyStatus[KeyEnum.RIGHT].pressed){
-			//angularSpeedDirection[1] = 1;
+		}
+		if (keyStatus[KeyEnum.RIGHT].pressed){
 			v3.add(angularSpeedDirection, upDirection, angularSpeedDirection);
 		}
 		if (keyStatus[KeyEnum.UP].pressed){
 			v3.add(angularSpeedDirection, leftDirection, angularSpeedDirection);
-		}else if (keyStatus[KeyEnum.DOWN].pressed){
+		}
+		if (keyStatus[KeyEnum.DOWN].pressed){
 			v3.subtract(angularSpeedDirection, leftDirection, angularSpeedDirection);
+		}
+		if (keyStatus[KeyEnum.Q].pressed){
+			v3.add(angularSpeedDirection, frontDirection, angularSpeedDirection);	
+		}
+		if (keyStatus[KeyEnum.E].pressed){
+			v3.subtract(angularSpeedDirection, frontDirection, angularSpeedDirection);
 		}
 
 		this.owner.motionComponent.angularSpeedDirection = angularSpeedDirection;
